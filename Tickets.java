b83051683e8b3d127f123ae49e6a7329c471908f
Rexoschulz <schulzoliver111208@gmail.com>
2025-11-21 16:55:53 +0000

    Tickets even more done

--- Tickets.java
+++ Tickets.java
@@ -10,6 +10,9 @@ public static void main(String[] args) {
   double numPavilion = 0.0;
   double numLawn = 0.0;
   double fee = 0.0;
+  int totalB = 0;
+  int totalP = 0;
+  int totalL = 0;
   double total = 0.00;
   System.out.println("What kinda ticket do you want.");
   String ticket = scan.nextLine();
@@ -23,20 +26,23 @@ public static void main(String[] args) {
     if(ticket.equals("B")){
       total = box + 1.50;
       numBox += box;
+      totalB += 1;
     }else if(ticket.equals("P")){
       total = pavilion + 1.50;
       numPavilion += pavilion;
+      totalP += 1;
     }else if(ticket.equals("L")){
       total = lawn + 1.50;
       numLawn += lawn;
+      totalL += 1;
     }
     fee += 1.50;
     System.out.println("What kinda ticket do you want.");
     ticket = scan.nextLine();
   }
-  System.out.println("Your total for box tickets is $" + numBox + "0.");
-  System.out.println("Your total for pavilion tickets is $" + numPavilion + "0.");
-  System.out.println("Your total for lawn tickets is $" + numLawn + "0.");
+  System.out.println("You have " + totalB + " box tickets. " + "Your total for box tickets is $" + numBox + "0.");
+  System.out.println("You have " + totalP + " pavilion tickets. " + "Your total for pavilion tickets is $" + numPavilion + "0.");
+  System.out.println("You have " + totalL + " lawn tickets. " + "Your total for lawn tickets is $" + numLawn + "0.");
   System.out.println("Your convenience fee is $" + fee + "0.");
   System.out.println("Your total is $" + total + "0");
   scan.close();
